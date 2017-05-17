9f22f4a3fbef21b715d0f85e6b918b948a847bd1
Jeremy Lincoln <bossco28@gmail.com>
2017-05-17 15:24:28 -0400

    logical operators with examples

--- Section9/logicaloperators.js
+++ Section9/logicaloperators.js
@@ -5,5 +5,28 @@ logicaloperators.js
 ||  9 > 9 || 5 === 5 true
 !   !(5 === 9) true
 
+*/
+
+var x = 10;
+var y = "a";
+
+console.log(y === "b" || x >= 10);
+
+
+var xx = 3;
+var yy = 8;
+
+console.log(!(xx == "3" || xx===yy) && !(yy != 8 && xx <= yy));
+
+/*
+Truthy and Falsy Values
+Falsy rule
+"", 0, NaN, -1, null, undefined
 
 */
+
+var str = "";
+var msg = "haha!";
+var isFunny = "false";
+console.log(((str || msg) && isFunny))
+console.log(!((str || msg) && isFunny));
