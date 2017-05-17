3d10b5505ea275152025d6a6e492a02078466cdf
Jeremy Lincoln <bossco28@gmail.com>
2017-05-17 15:04:51 -0400

    Boolean operators

---
+++ Section9/booleanlogic.js
@@ -0,0 +1,10 @@
+/*
+> - greater than
+< - less than
+>= - greater than or equal to
+<= - less than or equal to
+== - on value
+=== - equals on type
+!= - not equal to
+!== - not equal to or to type
+*/
