bfd89fac530f52562ce6b7657caff1d06769e4cd
Jeremy Lincoln <bossco28@gmail.com>
2017-05-19 10:41:30 -0400

    while loops

---
+++ Section9/loops.js
@@ -0,0 +1,16 @@
+var count = 1;
+
+while(count <= 10){
+    console.log("count is: " + count);
+    count++;
+}
+
+
+var str = "hello";
+
+var count = 0;
+
+while(count < str.length) {
+    console.log(str[count]);
+    count++;
+}
