901b7831c209fa0f453db31dc781af39ecb7caf2
Jeremy Lincoln <bossco28@gmail.com>
2017-05-19 14:41:12 -0400

    functions with arguments

---
+++ Section10/functionargs.js
@@ -0,0 +1,26 @@
+function square(num) {
+    console.log(num * num);
+}
+
+square(2);
+square(4);
+square(25);
+
+
+function area(length, width){
+    console.log(length * width);
+}
+
+
+area(2, 4);
+area(4, 23);
+area(12, 6);
+
+function greet(person1, person2, person3){
+    console.log("hi " + person1);
+    console.log("hi " + person2);
+    console.log("hi " + person3);
+}
+
+
+greet('Jeremy', 'Josh', 'Tim');
