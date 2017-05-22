d1f4b82639b348192f0d0be43a8fbaf52e7ea208
Jeremy Lincoln <bossco28@gmail.com>
2017-05-22 10:18:41 -0400

    Higher order functions

---
+++ Section10/highOrderFunction.js
@@ -0,0 +1,17 @@
+//higher order functions, either take a function as an argument or
+//return an other function
+
+
+function sing(){
+    console.log("twinkle twinkle..");
+    console.log("how I wonder..");
+}
+
+//the reason we don't need parentheses with the function is
+//that we aren't calling it.
+
+//setInterval(sing, 1000);
+
+// setInterval(function(){
+//     console.log("hi");
+// }, 1000)
