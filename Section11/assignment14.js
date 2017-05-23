16d83f1510eaaab046f90baa9247c79157839b49
Jeremy Lincoln <bossco28@gmail.com>
2017-05-23 16:21:12 -0400

    assignment 14

---
+++ Section11/assignment14.js
@@ -0,0 +1,11 @@
+var numbers = [22,67,33,96,88];
+console.log(numbers[numbers.length]);
+
+
+var friendGroups = [
+    ['Jeremy', 'Tim', 'Jimmy'],
+    ['Jeff', 'Tom', 'Kerry'],
+    ['Josh', 'kim', 'Joey'],
+]
+
+console.log(friendGroups[2][0]);
