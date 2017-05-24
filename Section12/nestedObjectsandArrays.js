0f74c9f72a9614fc2a674cd50a1e4fce9640e3ba
Jeremy Lincoln <bossco28@gmail.com>
2017-05-24 15:32:43 -0400

    Nested objects in an array

---
+++ Section12/nestedObjectsandArrays.js
@@ -0,0 +1,19 @@
+var posts = [
+    {
+        title: "Cats are mdeicore",
+        author: "Jeremy",
+        comments: [
+            "Awesome Post", "Terrible Post"
+        ]
+    },
+    {
+        title: "Cats are actually awesome",
+        author: "Mary",
+        comments: [
+            "Awesome Post Man", "Go to Hell"
+        ]
+    }
+]
+
+
+console.log(posts[0].title);
