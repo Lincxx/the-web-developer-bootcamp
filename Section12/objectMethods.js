9b0ee37b569418bbf613d1450a1b22d56c005288
Jeremy Lincoln <bossco28@gmail.com>
2017-05-24 16:41:26 -0400

    Add Methods, (functions) to an object

---
+++ Section12/objectMethods.js
@@ -0,0 +1,13 @@
+var obj = {
+    name: "Chuck",
+    age: 45,
+    isCool: false,
+    friends: ['bob', 'tina'],
+    add: function(x,y){
+        return x + y;
+    }
+}
+
+var dog = {}
+dog.speak = function(){return "Woof!";}
+console.log(dog.speak());
