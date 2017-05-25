09510fc223c8afe58f421f7cebca84ffd30a9f93
Jeremy Lincoln <bossco28@gmail.com>
2017-05-25 11:12:35 -0400

    Manipulation styles

---
+++ Section13/manipulatingstyle.js
@@ -0,0 +1,7 @@
+var h1 = document.querySelector('h1');
+//h1.style.border = "1px pink solid";
+
+var p = document.querySelector('p');
+//p.classList.add("big");
+//p.classList.remove("big");
+//p.classList.toggle("big");
