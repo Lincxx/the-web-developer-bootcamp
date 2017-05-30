5302ebf23cec6270daa37e19201496b987ade367
Jeremy Lincoln <bossco28@gmail.com>
2017-05-30 16:59:50 -0400

    Added 2 function, one to pick a random color and the other to make all the sqaures color when correct

--- Section15/colorgame.js
+++ Section15/colorgame.js
@@ -8,8 +8,10 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll('.square');
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
+var messageDisplay = document.querySelector('#message');
+
 colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
@@ -21,9 +23,25 @@ for (var i = 0; i < squares.length; i++) {
         var clickedColor = this.style.backgroundColor;
         //compare color to pickedColor
         if (clickedColor === pickedColor) {
-            alert('corrected');
+            messageDisplay.textContent = "Correct";
+            changeColors(clickedColor);
         } else {
-            alert('wrong');
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
         }
     });
+}
+
+function changeColors(color) {
+    //loop through all squares
+    for (var i = 0; i < colors.length; i++) {
+        //change each color to match given color
+        squares[i].style.backgroundColor = color;
+    }
+
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
