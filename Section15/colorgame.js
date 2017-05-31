f81e0c894d5a107a1e5c6dfe18882bb812ce4901
Jeremy Lincoln <bossco28@gmail.com>
2017-05-31 08:27:10 -0400

    Added in some true random colors now

--- Section15/colorgame.js
+++ Section15/colorgame.js
@@ -1,17 +1,13 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-];
+var colors = generateRandomColor(6);
 
 var squares = document.querySelectorAll('.square');
+//set a random color
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.querySelector('#message');
+var h1 = document.querySelector('h1');
 
+//set the color value, we wnat the user to see
 colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
@@ -24,6 +20,7 @@ for (var i = 0; i < squares.length; i++) {
         //compare color to pickedColor
         if (clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct";
+            h1.style.backgroundColor = clickedColor;
             changeColors(clickedColor);
         } else {
             this.style.backgroundColor = "#232323";
@@ -42,6 +39,31 @@ function changeColors(color) {
 }
 
 function pickColor() {
+    //get a random number based on the length of the colors array
     var random = Math.floor(Math.random() * colors.length);
+    //return the random color
     return colors[random];
+}
+
+function generateRandomColor(number){
+    //make an array
+    var arr = [];
+    //repeat number times
+    for (var i = 0; i < number; i++) {
+        //get ranodm ccolor and push to array
+        arr.push(randomColor());
+    }
+    //return array
+    return arr;
+}
+
+function randomColor(){
+    //pick a red from 0-255
+    var r = Math.floor(Math.random()* 256);
+    //pick a green from 0-255
+    var g = Math.floor(Math.random()* 256);
+    //pick a blue from 0-255
+    var b = Math.floor(Math.random()* 256);
+
+    return "rgb("+ r + ", " + g + ", " + b + ")";
 }
